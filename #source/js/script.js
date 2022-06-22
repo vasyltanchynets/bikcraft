96f25c59c50f780cb73d37067224953c7e7c9968
Vasyl Tanchynets <vasyltanchinets98@gmail.com>
2022-06-22 21:10:54 +0300

    website-bikcraft

---
+++ #source/js/script.js
@@ -0,0 +1,73 @@
+$(".header-menu__icon").click(function (event) {
+  $(this).toggleClass("active");
+  $(".header-menu").toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $("body").data("scroll", $(window).scrollTop());
+  }
+  $("body").toggleClass("lock");
+  if (!$(this).hasClass("active")) {
+    $("body,html").scrollTop(parseInt($("body").data("scroll")));
+  }
+});
+
+function ibg() {
+  $.each($(".ibg"), function (index, val) {
+    if ($(this).find("img").length > 0) {
+      $(this).css(
+        "background-image",
+        'url("' + $(this).find("img").attr("src") + '")'
+      );
+    }
+  });
+}
+ibg();
+
+// Spollers
+$(document).ready(function () {
+  $(".perguntas__spoiller-title").click(function (event) {
+    if ($(".perguntas__spoillers").hasClass("one")) {
+      $(".perguntas__spoiller-title").not($(this)).removeClass("active");
+      $(".perguntas__spoiller-text").not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
+});
+
+const animItems = document.querySelectorAll("._anim-items");
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_active");
+      } else {
+        if (!animItem.classList.contains("_anim-no-hide")) {
+          animItem.classList.remove("_active");
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+
+  setTimeout(() => {
+    animOnScroll();
+  }, 300);
+}
